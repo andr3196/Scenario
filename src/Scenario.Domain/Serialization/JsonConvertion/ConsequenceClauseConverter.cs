8b662f2da88223edee1ecaca371ad533f4708d6b
ansp <ansp@netcompany.com>
2022-06-06 15:08:03 +0200

    Ensure consistent serialization

--- src/Scenario.Domain/Serialization/JsonConvertion/ConsequenceClauseConverter.cs
+++ src/Scenario.Domain/Serialization/JsonConvertion/ConsequenceClauseConverter.cs
@@ -6,14 +6,20 @@ namespace Scenario.Domain.Serialization.JsonConvertion
 {
     public class ConsequenceClauseConverter : IConsequenceClauseConverter
     {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new PredicateClauseConverter() },
+        };
+        
         public string Serialize(ConsequenceClause clause)
         {
-            return JsonSerializer.Serialize(clause);
+            return JsonSerializer.Serialize(clause, options);
         }
 
         public ConsequenceClause Deserialize(string clauseJson)
         {
-            return JsonSerializer.Deserialize<ConsequenceClause>(clauseJson) ?? throw new InvalidOperationException();
+            return JsonSerializer.Deserialize<ConsequenceClause>(clauseJson, options) ?? throw new InvalidOperationException();
         }
     }
 }
