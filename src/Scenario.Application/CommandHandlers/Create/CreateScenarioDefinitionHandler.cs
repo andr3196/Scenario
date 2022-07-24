dcfd5be5cd3bac0f83fe1c7047647a1d5564162c
ansp <ansp@netcompany.com>
2022-07-24 21:13:35 +0200

    Added handler

---
+++ src/Scenario.Application/CommandHandlers/Create/CreateScenarioDefinitionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Scenario.Contracts.Commands;
+
+namespace Scenario.Application.CommandHandlers.Create
+{
+    public class CreateScenarioDefinitionHandler : IRequestHandler<CreateScenarioDefinition, Guid>
+    {
+        public Task<Guid> Handle(CreateScenarioDefinition request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Scenario.Contracts/Commands/CreateScenarioDefinition.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Scenario.Contracts.Commands
+{
+    public class CreateScenarioDefinition : IRequest<Guid>
+    {
+        
+        public CreateScenarioDefinition(string definition)
+        {
+            Definition = definition;
+        }
+        
+        public string Definition { get; }
+    }
+}
