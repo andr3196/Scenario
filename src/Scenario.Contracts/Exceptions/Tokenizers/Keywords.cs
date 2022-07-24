5a939f7c09a6c5372456344adf67eb3a33e2079d
ansp <ansp@netcompany.com>
2022-07-24 21:20:36 +0200

    Add keywords

---
+++ src/Scenario.Contracts/Exceptions/Tokenizers/Keywords.cs
@@ -0,0 +1,16 @@
+﻿namespace Scenario.Contracts.Exceptions.Tokenizers
+{
+    public static class Keywords
+    {
+        public const string When = "WHEN";
+        public const string Then = "THEN";
+        public const string Where = "WHERE";
+        
+        public const string Created = "CREATED";
+        public const string Updated = "UPDATED";
+        public const string Deleted = "DELETED";
+
+        public static string[] StateEventTypes => new[] { Created, Updated, Deleted};
+
+    }
+}
